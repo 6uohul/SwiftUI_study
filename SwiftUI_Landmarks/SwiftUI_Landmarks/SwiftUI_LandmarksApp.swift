0f99f23029107d0b3fd0d133cfba5d9f433e4e0f
6uohul <kiy010604@naver.com>
2022-10-11 04:14:29 +0900

    Tutorial(1)-Landmakrs 클론코딩

---
+++ SwiftUI_Landmarks/SwiftUI_Landmarks/SwiftUI_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LandmarksApp.swift
+//  SwiftUI_Landmarks
+//
+//  Created by 김인영 on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
