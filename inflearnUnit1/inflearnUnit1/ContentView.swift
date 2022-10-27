800ddc608713e942a369dffbcdfc9e3ad9e898e5
6uohul <kiy010604@naver.com>
2022-10-28 03:16:03 +0900

    [inflearn] 한 조각씩 배우는 SwiftUI - 입문편 클론코딩 시작

---
+++ inflearnUnit1/inflearnUnit1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  inflearnUnit1
+//
+//  Created by 김인영 on 2022/10/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
