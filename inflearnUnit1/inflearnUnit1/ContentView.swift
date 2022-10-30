50131ec46c3e0839d3d868d9c618783924d08645
6uohul <kiy010604@naver.com>
2022-10-31 01:03:03 +0900

    버튼을 눌렀을 때 bolt가 번쩍이는 화면 구현해보기

--- inflearnUnit1/inflearnUnit1/ContentView.swift
+++ inflearnUnit1/inflearnUnit1/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isLighting : Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.edgesIgnoringSafeArea(.all)
+            
+            
+            VStack {
+                Spacer()
+                
+                Image(systemName: isLighting ? "bolt.fill" : "bolt")
+                    .resizable()
+                    .foregroundColor(.red)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200)
+                
+                Spacer()
+                
+                HStack {
+                    Text("번쩍을 원한다면")
+                    Button {
+                        isLighting.toggle()
+                    } label: {
+                        Text("번쩍!")
+                            .foregroundColor(.red)
+                    }
+
+                }
+            }
         }
-        .padding()
     }
 }
 
